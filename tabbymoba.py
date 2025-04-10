d25cc7f647c416b30e73c096573ebe35154e4a73
wolfhowlmedia <56067495+wolfhowlmedia@users.noreply.github.com>
2025-04-10 11:43:36 +0200

    Add defaults and cli switches
    And prevent a crash if MobaXterm Sessions.mxtsessions doesn't exist

--- tabbymoba.py
+++ tabbymoba.py
@@ -1,21 +1,34 @@
 import yaml
 import configparser
+import argparse
 import os
 import re
 
 
 config = configparser.ConfigParser(interpolation=None, allow_no_value=True)
-config.optionxform=str
+
+parser = argparse.ArgumentParser(prog='TabbyToMobaXTerm')
+parser.add_argument("-i", "--input",
+    default="config.yaml",
+    help="Input file (default: config.yaml)")
+parser.add_argument("-o", "--output",
+    default="tabbytomobaxterm",
+    help="Output file (default: tabbytomobaxterm)")
 
 binds = {
     'ssh': '109',
     'local': '151',
     'serial': '131'
 }
+args = parser.parse_args()
 
 data = {}
-with open('tabby.yaml') as f:
-    data = yaml.load(f, Loader=yaml.FullLoader)
+if not os.path.exists(args.input):
+    print(f"{args.input} doesn't exist. Aborting!")
+    raise SystemExit
+
+with open(args.input) as f:
+    data = yaml.load(f, Loader=yaml.SafeLoader)
 
 items = {'ungrouped': []}
 for profile in data['profiles']:
@@ -31,7 +44,7 @@ for profile in data['profiles']:
         entry['host'] = profile['options']['host'] if 'host' in profile['options'] else 'localhost'
         entry['user'] = profile['options']['user'] if 'user' in profile['options'] else 'root'
         entry['port'] = profile['options']['port'] if 'port' in profile['options'] else '22'
-    
+
     if 'group' in profile:  # Grouped stuff
         if not profile['group'] in  items:
             items[profile['group']] = [entry]
@@ -40,11 +53,11 @@ for profile in data['profiles']:
     else:  # Ungrouped stuff
         items['ungrouped'].append(entry)
 
-print(items)
-
 sessions_file = 'MobaXterm Sessions.mxtsessions'
 if os.path.exists(sessions_file):
     config.read(sessions_file)
+else:
+    print("MobaXterm Sessions.mxtsessions not found. Creating file from scratch.")
 
 sections = config.sections()
 sections.sort(reverse=True)
@@ -53,8 +66,7 @@ last_section = 1
 try:
     last_section = int(re.sub(r'^.*_(\d+)$', r'\1', sections[0]))
 except IndexError:
-    print("Yeah, something went wrong")
-    raise SystemExit
+    last_section = 0
 
 section_id = last_section + 1
 config.add_section(f'Bookmarks_{section_id}')
@@ -73,7 +85,7 @@ for idx, key in enumerate(items):
             entry += "% %-1%-1% %%22%%0%0%Interactive shell%"
             config[f'Bookmarks_{section_id}'][item['name']] = entry
 
-
-# print(config.sections())
-with open('test.mxtsessions', 'w') as out:
-    print(config.write(out))
+outputfilename = args.output + (".mxtsessions" if not args.output.endswith(".mxtsessions") else "")
+with open(outputfilename, 'w') as out:
+    config.write(out)
+    print(f"{outputfilename} written.")
